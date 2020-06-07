2d223a0c03aabc60273b9b4b18335eb2e2f9a8f0
Bruno <bruno.pereira@sonner.com.br>
2020-06-07 18:08:21 -0300

    Credenciais de login sendo enviadas corretamente

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,28 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController, MenuController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {NavController, MenuController} from '@ionic/angular';
+import {CredenciaisDTO} from '../../models/credenciais.dto';
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+    selector: 'app-folder',
+    templateUrl: './folder.page.html',
+    styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+    public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute, public navCtrl: NavController,  public menu: MenuController ) { }
+    credenciais: CredenciaisDTO = {
+        email: '',
+        senha: ''
+    };
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-  }
 
-  login(){
-    this.navCtrl.setDirection('root');
-  }
-  ionViewWillEnter() {
-    this.menu.enable(false);
+    constructor(private activatedRoute: ActivatedRoute, public navCtrl: NavController, public menu: MenuController) {
     }
+
+    ngOnInit() {
+        this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    }
+
+    login() {
+        console.log(this.credenciais);
+        this.navCtrl.setDirection('root');
+    }
+
+    cadastra() {
+        //dados estao chegando aqui normalmente;
+        console.log(this.credenciais);
+        // this.navCtrl.setDirection('root');
+    }
+
+
+    ionViewWillEnter() {
+        this.menu.enable(false);
+    }
+
     ionViewDidLeave() {
-    this.menu.enable(true);
+        this.menu.enable(true);
     }
 }
