6d378121d016c4e702af5bf4f88f84e53eea0a49
FellipeMarraa <fellipemarra@hotmail.com>
2020-06-07 14:01:48 -0300

    biotechapp

---
+++ src/app/categorias/categorias.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from 'src/services/domain/categoria.service';
+import { CategoriaDTO } from 'src/models/categoria.dto';
+import { API_CONFIG } from 'src/config/api.config';
+
+@Component({
+  selector: 'app-categorias',
+  templateUrl: './categorias.page.html',
+  styleUrls: ['./categorias.page.scss'],
+})
+export class CategoriasPage implements OnInit {
+
+  imgUrl : string = API_CONFIG.imgBaseUrl;
+
+  items: CategoriaDTO[];
+
+  constructor(public categoriaService : CategoriaService) { }
+
+  ngOnInit() {
+    this.categoriaService.findAll().subscribe(response => {this.items = response;}, error => {console.log(error);});
+  }
+
+}
---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+  public folder: string;
+
+  constructor(private activatedRoute: ActivatedRoute, public navCtrl: NavController,  public menu: MenuController ) { }
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  login(){
+    this.navCtrl.setDirection('root');
+  }
+  ionViewWillEnter() {
+    this.menu.enable(false);
+    }
+    ionViewDidLeave() {
+    this.menu.enable(true);
+    }
+}
