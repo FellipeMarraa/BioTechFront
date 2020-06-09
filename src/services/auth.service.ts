59eb72aacac9061e9d79b3a84cf88a515a85f8b4
Bruno <bruno.pereira@sonner.com.br>
2020-06-09 17:04:39 -0300

    TesteExercicio2

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -20,11 +20,11 @@ export class AuthService {
     }
 
     sucessfullLogin(authorizationValue: string) {
-        let tok = authorizationValue.substring(7);
-        let user: LocalUser = {
-            token: tok
-        };
-        this.storage.setLocalUser(user);
+        // let tok = authorizationValue.substring(7);
+        // let user: LocalUser = {
+        //     token: tok
+        // };
+        // this.storage.setLocalUser(user);
     }
 
     logout() {
